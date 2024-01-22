01fcaae4a4fe09a11c5ee3bf3c5af1ed3b0ee3bf
Parmod Kumar <yparmodyadav@gmail.com>
2024-01-22 22:06:18 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@
 // Imports
 import Navbar from "./Components/Navbar/Navbar";
 import AlbumsList from "./Components/Albums List/Albums-List";
+import { useEffect, useState } from "react";
 
 function App() {
+  // States
+  const [albums, setAlbums] = useState([]);
+
+  // Side effects
+  useEffect(() => {
+    // Fetching data here
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/albums"
+      );
+      const albums = await response.json();
+      // Setting state
+      setAlbums(albums);
+    };
+  }, []);
+
+
   return (
     <>
-    <Navbar/>
-    <AlbumsList/>
+      <Navbar />
+      <AlbumsList />
     </>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,9 @@ import styles from "./Navbar.module.css";
 export default function Navbar(){
     // Returning JSX
     return(
-        <>
-        </>
+        <div className={styles.navbarContainer}>
+            <p className={styles.item}>Albums</p>
+            <p className={styles.item}>Add Album</p>
+        </div>
     )
 }
