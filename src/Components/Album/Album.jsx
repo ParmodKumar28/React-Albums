8c1564ab96298e1c39773a58ced5e4b0f98b4e53
Parmod Kumar <yparmodyadav@gmail.com>
2024-01-22 23:13:55 +0530

    Feature added

--- src/Components/Album/Album.jsx
+++ src/Components/Album/Album.jsx
@@ -1,12 +1,14 @@
 // Imports
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./Album.module.css";
 
 // Component Here
 export default function Album({ id, userId, title }) {
+  const [visible, setVisible] = useState(false);
+
   // Returning JSX
   return (
-    <div className={styles.albumContainer}>
+    <div className={styles.albumContainer} onMouseOver={() => setVisible(true)} onMouseLeave={() => setVisible(false)}>
       <div className={styles.imageContainer}>
         <img
           src="https://cdn-icons-png.flaticon.com/128/8371/8371705.png"
@@ -17,6 +19,12 @@ export default function Album({ id, userId, title }) {
       <div className={styles.albumInfo}>
         <p className={styles.title}>{title}</p>
       </div>
+
+      {/* Delete update icon container */}
+      {visible && <div className={styles.iconContainer}>
+        <img src="https://cdn-icons-png.flaticon.com/128/6460/6460112.png" alt="delete" className={styles.icon} />
+        <img src="https://cdn-icons-png.flaticon.com/128/12493/12493756.png" alt="update" className={styles.icon} />
+      </div>}
     </div>
   );
 }
